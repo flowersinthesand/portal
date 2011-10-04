e6ae745f1eeddb062106178787375014f9d7710d
Donghwan Kim <flowersinthesand@gmail.com>
2011-10-04 17:56:59 +0900

    Start jQuery Socket

---
+++ jquery.socket.js
@@ -0,0 +1,11 @@
+/*
+ * jQuery Socket
+ * http://github.com/flowersinthesand/jquery-socket
+ * 
+ * Copyright 2011, Donghwan Kim 
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+(function($, undefined) {
+
+})(jQuery);
