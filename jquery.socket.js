117f329d0bced59182a77d4d801a352af3f9b146
Donghwan Kim <flowersinthesand@gmail.com>
2011-10-23 22:10:23 +0900

    Fixes #3 - Test transport

--- jquery.socket.js
+++ jquery.socket.js
@@ -9,7 +9,9 @@
 (function($, undefined) {
 
 	var // Sockets
-		sockets = {};
+		sockets = {},
+		// Transports
+		transports = {};
 	
 	// Socket is based on The WebSocket API 
 	// W3C Working Draft 29 September 2011 - http://www.w3.org/TR/2011/WD-websockets-20110929/
@@ -22,7 +24,6 @@
 					return sockets[i];
 				}
 			}
-			
 			return null;
 		}
 		
@@ -31,18 +32,27 @@
 			return sockets[url];
 		}
 		
-		var // Socket object
+		var // Transport
+			transport,
+			// Socket object
 			socket = {
 				// Final options object
-				options: $.extend(true, {}, options),
+				options: $.extend(true, {
+					type: "test"
+				}, options),
 				// Transmits data using the connection
 				send: function(data) {
-					return socket.open(function(/* TODO remove -- just for tests */ send) {
-						send(data);
+					return socket.open(function() {
+						if (transport) {
+							transport.send(data);
+						}
 					});
 				},
 				// Disconnects the connection
 				close: function(code, reason) {
+					if (transport) {
+						transport.close(code, reason);
+					}
 					delete sockets[url];
 					return socket;
 				},
@@ -61,11 +71,13 @@
 			var old = socket[type];
 			
 			function handler(fn) {
+				var args = arguments;
+				
 				if (old && !$.isFunction(fn)) {
-					return old.apply(socket, arguments);
+					return old.apply(socket, args);
 				}
 				
-				handler.add(fn);
+				handler.add.apply(handler, args);
 				
 				return socket;
 			}
@@ -73,13 +85,99 @@
 			socket[type] = $.extend(handler, $.Callbacks(flags));
 		});
 		
+		if (socket.options.type in transports) {
+			transport = transports[socket.options.type](socket);
+			transport.open();
+		}
+		
 		// The url is a identifier of this socket within the document
 		sockets[url] = socket;
 		
 		return socket;
 	};
+	
+	$.extend(transports, {
+		// The server-side view of the socket handling
+		test: function(socket) {
+			var // Is it accepted?
+				accepted,
+				// Creates a function delaying its execution to simulate network delay
+				delay = function(fn) {
+					return function() {
+						var context = this,
+							args = arguments;
+						
+						setTimeout(function() {
+							fn.apply(context, args);
+						}, 10);
+						
+						return context;
+					};
+				},
+				// Request object for the server
+				request = {
+					accept: function() {
+						accepted = true;
+						return connection;
+					},
+					reject: function() {
+						accepted = false;
+					}
+				},
+				// Connection object for the server
+				connection = {
+					send: delay(function(data) {
+						if (socket.open.fired()) {
+							socket.message.fire(data);
+						}
+					}),
+					close: delay(function() {
+						if (socket.open.fired()) {
+							socket.close.fire();
+						}
+					}),
+					on: function(type, fn) {
+						event.on(type, function() {
+							fn.apply(connection, Array.prototype.slice.call(arguments, 1));
+						});
+						return connection;
+					}
+				},
+				// Connection event helper
+				event = $({});
+			
+			if (socket.options.server) {
+				socket.options.server(request);
+			}
+			
+			return {
+				open: delay(function() {
+					switch (accepted) {
+					case true:
+						socket.open.fire();
+						event.triggerHandler("open");
+						break;
+					case false:
+						socket.close.fire();
+						break;
+					}
+				}),
+				send: delay(function(data) {
+					if (accepted) {
+						event.triggerHandler("message", [data]);
+					}
+				}),
+				close: delay(function(code, reason) {
+					if (accepted) {
+						socket.close.fire();
+						event.triggerHandler("close", [code, reason]);
+					}
+				})
+			};
+		}
+	});
 
-	$(window).bind("unload.socket", function() {
+	$(window).on("unload.socket", function() {
 		for (var url in sockets) {
 			if (sockets.hasOwnProperty(url)) {
 				delete sockets[url];
--- test/test.js
+++ test/test.js
@@ -59,15 +59,25 @@ $.each(["open", "message", "error", "close"], function(i, name) {
 	});
 });
 
-test("send method should defer sending message when the socket is not connected", function() {
+asyncTest("send method should defer sending message when the socket is not connected", function() {
 	var result = "";
 	
-	$.socket("url").send("A").send("B").open.fire(function(string) {
-		result += string;
+	$.socket("url", {
+		server: function(request) {
+			request.accept().on("message", function(data) {
+				result += data;
+				if (result == "ABC") {
+					ok(true);
+					start();
+				}
+			});
+		}
+	})
+	.send("A")
+	.send("B")
+	.open(function() {
+		$.socket().send("C");
 	});
-	$.socket("url").send("C");
-	
-	equal(result, "ABC");
 });
 
 test("close method should delete socket reference", function() {
@@ -78,4 +88,149 @@ test("close method should delete socket reference", function() {
 
 test("find method should find a logical sub socket", function() {
 	ok($.socket("url").find("chat"));
+});
+
+module("Transport test", {
+	teardown: teardown
+});
+
+asyncTest("server should be executed with request", function() {
+	$.socket("url", {
+		server: function(request) {
+			ok(request);
+			start();
+		}
+	});
+});
+
+asyncTest("request should be pended if there is no action on request", function() {
+	$.socket("url", {
+		server: function(request) {
+			ok(request);
+		}
+	})
+	.open(function() {
+		ok(false);
+	})
+	.close(function() {
+		ok(false);
+	});
+	
+	setTimeout(start, 100);
+});
+
+asyncTest("request's accept method should return connection object and fire open event", function() {
+	$.socket("url", {
+		server: function(request) {
+			ok(request.accept());
+		}
+	})
+	.open(function() {
+		ok(true);
+	}, start)
+	.close(function() {
+		ok(false);
+	});
+});
+
+asyncTest("request's reject method should fire close event", function() {
+	$.socket("url", {
+		server: function(request) {
+			ok(!request.reject());
+		}
+	})
+	.open(function() {
+		ok(false);
+	})
+	.close(function() {
+		ok(true);
+	}, start);
+});
+
+asyncTest("connection's send method should fire message event", function() {
+	$.socket("url", {
+	    server: function(request) {
+	    	var connection = request.accept();
+	    	connection.on("open", function() {
+            	connection.send("data");
+            });
+	    }
+	})
+	.message(function(data) {
+		equal(data, "data");
+	}, start);
+});
+
+asyncTest("connection's close method should fire close event", function() {
+	$.socket("url", {
+	    server: function(request) {
+	    	var connection = request.accept();
+	    	connection.on("open", function() {
+            	connection.close();
+            });
+	    }
+	})
+	.close(function() {
+		ok(true);
+	}, start);
+});
+
+asyncTest("connection's open event should be fired after socket's open event", function() {
+	var result = "";
+	
+	$.socket("url", {
+	    server: function(request) {
+	    	request.accept().on("open", function() {
+            	result += "B";
+            	equal(result, "AB");
+            	start();
+            });
+	    }
+	})
+	.open(function() {
+		result += "A";
+	});
+});
+
+asyncTest("connection's message event handler should receive a data sent by the socket", function() {
+	$.socket("url", {
+	    server: function(request) {
+	    	request.accept().on("message", function(data) {
+            	equal(data, "Hello");
+            	start();
+            });
+	    }
+	})
+	.send("Hello");
+});
+
+asyncTest("connection's close event should be fired after socket's close event", function() {
+	var result = "";
+	
+	$.socket("url", {
+	    server: function(request) {
+	    	request.accept().on("close", function() {
+        		result += "B";
+            	equal(result, "AB");
+            	start();
+            })
+	    }
+	})
+	.close(function() {
+		result += "A";
+	})
+	.close();
+});
+
+asyncTest("connection's close event handler should receive close code and reason", function() {
+	$.socket("url", {
+	    server: function(request) {
+	    	request.accept().on("close", function(code, reason) {
+            	ok(code, 1000);
+            	ok(reason, "normal");
+            	start();
+            });
+	    }
+	})
+	.close(1000, "normal");
 });
