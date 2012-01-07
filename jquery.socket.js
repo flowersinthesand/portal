19d7a1fe1b05976f771f3375a4b213e2d7b01c8a
Donghwan Kim <flowersinthesand@gmail.com>
2012-01-08 01:43:19 +0900

    Update the copyright year to 2012

--- jquery.socket.js
+++ jquery.socket.js
@@ -2,7 +2,7 @@
  * jQuery Socket
  * http://github.com/flowersinthesand/jquery-socket
  * 
- * Copyright 2011, Donghwan Kim 
+ * Copyright 2012, Donghwan Kim 
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
