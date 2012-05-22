f16b48a4acb99fd6759e44fb20b3e48a537cbfb4
Donghwan Kim <flowersinthesand@gmail.com>
2012-05-22 21:34:04 +0900

    Use one tab instead of spaces

--- jquery.socket.js
+++ jquery.socket.js
@@ -634,7 +634,7 @@
 				var r = Math.random() * 16 | 0,
 					v = c === "x" ? r : (r & 0x3 | 0x8);
 				
-			    return v.toString(16);
+				return v.toString(16);
 			});
 		},
 		url: function(url, params) {
@@ -666,8 +666,8 @@
 			
 			// String.prototype.split is not reliable cross-browser
 			while (match = reol.exec(chunk)) {
-			    lines.push(chunk.substring(i, match.index));
-			    i = match.index + match[0].length;
+				lines.push(chunk.substring(i, match.index));
+				i = match.index + match[0].length;
 			}
 			lines.push(chunk.length === i ? "" : chunk.substring(i));
 			
